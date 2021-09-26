ca5259381d8cce2bba7ccdb28587bc9358348a50
lucas barros <lucasbarrosrock@hotmail.com>
2021-09-26 13:02:35 -0300

    Atualização da calculadora, separando em duas classes distintas.
    Adicionado método de log através de um arquivo JSON, registrando todas as operações realizadas.

---
+++ BibliotecaCalculadora/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+
+namespace ProgramaCalculadora
+{
+    public class Calculadora
+    {
+
+        JsonWriter writer;
+        public Calculadora()
+        {
+            StreamWriter logFile = File.CreateText("calculatorLog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operações");
+            writer.WriteStartArray();
+
+        }
+        public  double DoOperation(double num1, double num2, string op)
+        {
+
+            double resultado = double.NaN; // valor padrão é um "não-numero" se uma operação, como divisão, poderá resultar em um erro.
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operador1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operador1");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operação");
+            //usar um switch case para fazer o cálculo matemático
+
+            switch (op)
+            {
+                case "a":
+                    resultado = num1 + num2;
+                    writer.WriteValue("Adição");
+                    break;
+                case "s":
+                    resultado = num1 - num2;
+                    writer.WriteValue("Adição");
+                    break;
+                case "m":
+                    resultado = num1 * num2;
+                    writer.WriteValue("Multiplicação");
+                    break;
+                case "d":
+                    //peça ao usuário para colocar um numero não divisor por 0.
+                    if (num2 != 0)
+                    {
+                        resultado = num1 / num2;
+                        writer.WriteValue("Divisão");
+                    }
+                    break;
+                // retorne texto para uma entrada incorreta.
+                default:
+                    break;
+            }
+
+            writer.WritePropertyName("Resultado");
+            writer.WriteValue(resultado);
+            writer.WriteEndObject();
+            return resultado;
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+
+        }
+        
+    }
+}
--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -1,115 +1,86 @@
 ﻿using System;
 
-namespace Calculadora
+
+namespace ProgramaCalculadora
 {
-    class Calculadora
+    class Program
     {
-        public static double DoOperation(double num1, double num2, string op)
+        static void Main(string[] args)
         {
+            bool endApp = false;
+            //mostra titulo como um programa c# de calculadora.
+            Console.WriteLine("Calculadora de console em C#\r");
+            Console.WriteLine("-----------------------\n");
 
-            double resultado = double.NaN; // valor padrão é um "não-numero" se uma operação, como divisão, poderá resultar em um erro.
+            Calculadora calculadora = new Calculadora();
 
-            //usar um switch case para fazer o cálculo matemático
+            while (!endApp)
+            {    //declara as variáveis e colaca elas com valor 0.
+                string numInput1 = "";
+                string numInput2 = "";
+                double resultado = 0;
 
-            switch (op)
-            {
-                case "a":
-                    resultado = num1 + num2;
-                    break;
-                case "s":
-                    resultado = num1 + num2;
-                    break;
-                case "m":
-                    resultado = num1 + num2;
-                    break;
-                case "d":
-                    //peça ao usuário para colocar um numero não divisor por 0.
-                    if (num2 != 0)
-                    {
-                        resultado = num1 / num2;
-                    }
-                    break;
-                // retorne texto para uma entrada incorreta.
-                default:
-                    break;
-            }
-            return resultado;
-        }
+                //pergunta ao usuário para inserir o primeiro número
+                Console.WriteLine("Insira um número, depois pressione Enter: ");
+                numInput1 = Console.ReadLine();
 
-        class Program
-        {
-            static void Main(string[] args)
-            {
-                bool endApp = false;
-                //mostra titulo como um programa c# de calculadora.
-                Console.WriteLine("Calculadora de console em C#\r");
-                Console.WriteLine("-----------------------\n");
-
-                while (!endApp)
-                {    //declara as variáveis e colaca elas com valor 0.
-                    string numInput1 = "";
-                    string numInput2 = "";
-                    double resultado = 0;
-
-                    //pergunta ao usuário para inserir o primeiro número
-                    Console.WriteLine("Insira um número, depois pressione Enter: ");
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.WriteLine("Este não é um valor válido. Por favor coloque um número inteiro: ");
                     numInput1 = Console.ReadLine();
-
-                    double cleanNum1 = 0;
-                    while (!double.TryParse(numInput1, out cleanNum1))
-                    {
-                        Console.WriteLine("Este não é um valor válido. Por favor coloque um número inteiro: ");
-                        numInput1 = Console.ReadLine();
-                    }
-
-                    //pergunta ao usuário para inserir o segundo número
-                    Console.Write("Insira um outro número, depois pressione Enter: ");
-                    numInput2 = Console.ReadLine();
-
-                    double cleanNum2 = 0;
-                    while (!double.TryParse(numInput2, out cleanNum2))
-                    {
-                        Console.WriteLine("Este não é um valor válido. Por favor coloque um número inteiro: ");
-                        numInput2 = Console.ReadLine();
-                    }
-
-                    //pergunte ao usuário qual operação realizar.
-                    Console.WriteLine("Escolha uma opção da lista abaixo:");
-                    Console.WriteLine("\ta - Adição");
-                    Console.WriteLine("\ts - Subtração");
-                    Console.WriteLine("\tm - Multiplicação");
-                    Console.WriteLine("\td - Divisão");
-                    Console.WriteLine("Qual opção? ");
-
-                    string op = Console.ReadLine();
-
-                    try
-                    {
-
-                        resultado = Calculadora.DoOperation(cleanNum1, cleanNum2, op);
-                        if (double.IsNaN(resultado))
-                        {
-                            Console.WriteLine("Esse operador resultará em um erro matemático \n");
-                        }
-                        else Console.WriteLine("Seu resultado: {0:0.##}\n", resultado);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Aconteceu um erro! Uma exceção foi encontrada tentando realizar o calculo.\n - Detalhes: " +e.Message);
-                    }
-
-                    Console.WriteLine("---------------------\n");
-
-                    //aguarda o usuário responder antes de fechar.
-                    Console.WriteLine("Pressione 'n' e Enter para fechar o aplicativo, ou pressiona qualquer tecla e Enter para continuar: ");
-                    if (Console.ReadLine() == "n") endApp = true;
-
-                    Console.WriteLine("\n"); //pula linha.
                 }
-                return;
-            } 
 
+                //pergunta ao usuário para inserir o segundo número
+                Console.Write("Insira um outro número, depois pressione Enter: ");
+                numInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.WriteLine("Este não é um valor válido. Por favor coloque um número inteiro: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+                //pergunte ao usuário qual operação realizar.
+                Console.WriteLine("Escolha uma opção da lista abaixo:");
+                Console.WriteLine("\ta - Adição");
+                Console.WriteLine("\ts - Subtração");
+                Console.WriteLine("\tm - Multiplicação");
+                Console.WriteLine("\td - Divisão");
+                Console.WriteLine("Qual opção? ");
+
+                string op = Console.ReadLine();
+
+                try
+                {
+
+                    resultado = calculadora.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(resultado))
+                    {
+                        Console.WriteLine("Esse operador resultará em um erro matemático \n");
+                    }
+                    else Console.WriteLine("Seu resultado: {0:0.##}\n", resultado);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Aconteceu um erro! Uma exceção foi encontrada tentando realizar o calculo.\n - Detalhes: " + e.Message);
+                }
+
+                Console.WriteLine("---------------------\n");
+
+                //aguarda o usuário responder antes de fechar.
+                Console.WriteLine("Pressione 'n' e Enter para fechar o aplicativo, ou pressiona qualquer tecla e Enter para continuar: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n"); //pula linha.
+            }
+
+            // Adiciona uma chamada para fechar o JSON antes do retorno
+            calculadora.Finish();
+            return;
         }
+
     }
 }
 
